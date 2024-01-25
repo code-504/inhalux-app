228b03eed997e416adb8ddbc999a4bedf68732aa
code-504 <58441533+code-504@users.noreply.github.com>
2024-01-24 22:07:57 -0600

    Cambios resapldo

--- stores/inhaler.tsx
+++ stores/inhaler.tsx
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { Device } from "@/interfaces/Device";
+import { persist } from "zustand/middleware";
 
 const useInhalerStore = create<Device>((set) => ({
     inhalers: null,
